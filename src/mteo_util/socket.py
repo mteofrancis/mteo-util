38fac8eaea281cbc91cc385b4ac06b5fac96a43f
Francis M <francism@destinatech.com>
2021-09-03 20:11:11 +0100

    src/mteo_util/socket.py: import TcpSocket from fake-httpd package

---
+++ src/mteo_util/socket.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##
+# mteo-util.git:/src/mteo_util/socket.py
+##
+
+## {{{ ---- [ Header ] -----------------------------------------------------------------------------
+
+##
+# Copyright (c) 2021 Francis M <francism@destinatech.com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2.0 as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to:
+#
+#   Free Software Foundation
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA 02110
+#   USA
+##
+
+## }}} ---- [ Header ] -----------------------------------------------------------------------------
+
+import socket
+
+## {{{ class TcpSocket
+
+class TcpSocket:
+
+  # Instance of socket
+  _socket = None
+
+  # Underlying file descriptor
+  _fd = None
+
+  ## {{{ TcpSocket.__init__()
+  def __init__(self, sock=None):
+    if sock is None:
+      self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+      self._socket = sock
+
+    self._fd = self._socket.fileno()
+  ## }}}
+
+  ## {{{ TcpSocket.setblocking()
+  def setblocking(self, block=True):
+    self._socket.setblocking(block)
+  ## }}}
+
+  ## {{{ TcpSocket.bind()
+  def bind(self, address, port, reuse_addr=False):
+    if reuse_addr:
+      self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    self._socket.bind((address, port))
+  ## }}}
+
+  ## {{{ TcpSocket.listen()
+  def listen(self, backlog=10):
+    self._socket.listen(backlog)
+  ## }}}
+
+  ## {{{ TcpSocket.accept()
+  def accept(self):
+    return self._socket.accept()
+  ## }}}
+
+  ## {{{ TcpSocket.recv()
+  def recv(self, size, flags=0):
+    if flags != 0:
+      return self._socket.recv(size)
+    else:
+      return self._socket.recv(size, flags)
+  ## }}}
+
+  ## {{{ TcpSocket.shutdown()
+  def shutdown(self, how=socket.SHUT_RDWR):
+    self._socket.shutdown(how)
+  ## }}}
+
+  ## {{{ TcpSocket.close()
+  def close(self):
+    self._socket.close()
+    self._socket = None
+    self._fd = None
+  ## }}}
+
+## class TcpSocket }}}
+
+##
+# vim: ts=2 sw=2 tw=100 et fdm=marker :
+##
