dd52c89274aea3805f6f42ee0a47f7b985a9f016
Francis M <francism@destinatech.com>
2021-09-09 20:46:54 +0100

    Add new OsUser and OsGroup API

--- src/mteo_util/tests/test_os.py
+++ src/mteo_util/tests/test_os.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 ##
-# mteo-util.git:/<FILE>
+# mteo-util.git:/src/mteo_util/tests/test_os.py
 ##
 
 ## {{{ ---- [ Header ] -----------------------------------------------------------------------------
@@ -30,16 +30,42 @@
 
 ## }}} ---- [ Header ] -----------------------------------------------------------------------------
 
-from .bitmask import *
-from .buffer import *
-from .io import *
-from .misc import *
-from .os import *
-from .random import *
-from .socket import *
-from .string import *
-from .time import *
-from .typing import *
+import unittest
+
+from mteo_util import (
+  OsUser,
+  OsGroup,
+)
+
+class TestOsUser(unittest.TestCase):
+
+  def test_init_name_kwarg(self):
+    user = OsUser(name='root')
+    self.assertTrue(user.name == 'root')
+    self.assertTrue(user.passwd == 'x')
+    self.assertTrue(user.uid == 0)
+    self.assertTrue(user.gid == 0)
+
+  def test_init_uid_kwarg(self):
+    user = OsUser(uid=0)
+    self.assertTrue(user.name == 'root')
+    self.assertTrue(user.passwd == 'x')
+    self.assertTrue(user.uid == 0)
+    self.assertTrue(user.gid == 0)
+
+class TestOsGroup(unittest.TestCase):
+
+  def test_init_name_kwarg(self):
+    group = OsGroup(name='root')
+    self.assertTrue(group.name == 'root')
+    self.assertTrue(group.passwd == 'x')
+    self.assertTrue(group.gid == 0)
+
+  def test_init_uid_kwarg(self):
+    group = OsGroup(gid=0)
+    self.assertTrue(group.name == 'root')
+    self.assertTrue(group.passwd == 'x')
+    self.assertTrue(group.gid == 0)
 
 ##
 # vim: ts=2 sw=2 tw=100 et fdm=marker :
